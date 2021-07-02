493a24f14fbfe58c05be74dd2264c735fb88b6f2
Jeong Da Un <wjekdns019@likelion.org>
2021-07-02 12:07:19 +0900

    7/2 4ban Commit2

--- accountapp/views.py
+++ accountapp/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def hello_world(request):
-    return HttpResponse('Hello World!')     # HttpResponse 라는 객체를 반환    // 여기에 출력
+    return  render(request,'base.html')    # HttpResponse 라는 객체를 반환    // 여기에 출력
                                             # 붉은색 표시일때 alt+enter 눌러서 from.... 누르면 import 할 필요 x
 
 
