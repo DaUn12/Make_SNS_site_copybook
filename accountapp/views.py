7d9d70a32c734c16cdbab24f7e78e18d7f2a357b
Jeong Da Un <wjekdns019@likelion.org>
2021-07-09 11:52:03 +0900

    7/9 4ban Commit 1

--- accountapp/views.py
+++ accountapp/views.py
@@ -4,8 +4,16 @@ from django.shortcuts import render
 # Create your views here.
 
 def hello_world(request):
-    return  render(request,'accountapp/hello_world.html')    # HttpResponse 라는 객체를 반환    // 여기에 출력
-                                                        # 붉은색 표시일때 alt+enter 눌러서 from.... 누르면 import 할 필요 x
+    #request 함수에 method 라는 저장공간이 있음
+    if request.method == 'POST':
+        return  render(request,'accountapp/hello_world.html'
+                       ,context={'text':'POST METHOD!'})
+     # HttpResponse 라는 객체를 반환    // 여기에 출력
+     # 붉은색 표시일때 alt+enter 눌러서 from.... 누르면 import 할 필요 x
+
+
+    else:
+        return render(request, 'accountapp/hello_world.html',context={'text': 'GET METHOD!'})
 
 
 
