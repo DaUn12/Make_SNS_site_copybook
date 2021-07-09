1b50c5c630551df7fc537a0cfe31bd91bca94b5a
Jeong Da Un <wjekdns019@likelion.org>
2021-07-09 11:21:49 +0900

    7/9 4ban Commit 1

--- accountapp/models.py
+++ accountapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class NewModel(models.Model):
+    text = models.CharField(max_length=255,null=False)
+    # charfield : 문자열 받음 ,max_length : 최대 문자열 255, null = 공란의 여부
+
+
+
+
+
+
