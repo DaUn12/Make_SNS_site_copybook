e86842dbddda3a2a1551a1e0642ca65c78048d1b
Jeong Da Un <wjekdns019@likelion.org>
2021-07-27 11:16:41 +0900

    7/27  4ban Commit 1

---
+++ profileapp/forms.py
@@ -0,0 +1,18 @@
+from django.forms import ModelForm
+
+
+# 모델 폼이라는 걸 상속하여 클래스를 만듬
+from profileapp.models import Profile
+
+
+class ProfileCreationForm(ModelForm):
+    class Meta:
+        model =Profile      # profileapp- model.py 에있는  profile 이라는 클래스를 받음
+        fields = ['image', 'nickname', ',message']
+    # fields 에서 user 를 안받는 이유는 서버측에서 관리할 것이기 때문에
+
+
+
+
+
+
--- profileapp/models.py
+++ profileapp/models.py
@@ -3,6 +3,10 @@ from django.db import models
 
 # Create your models here.
 
+
+
+
+
 class Profile(models.Model):
 
     user = models.OneToOneField(User, on_delete= models.CASCADE,
