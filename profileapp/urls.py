75b5d6953cc9b76515c900492e1681709cdcd822
Jeong Da Un <wjekdns019@likelion.org>
2021-07-27 11:49:16 +0900

    7/27  4ban Commit 1

--- profileapp/urls.py
+++ profileapp/urls.py
@@ -1,4 +1,15 @@
+from django.urls import path
+
+from profileapp.views import ProfileCreateView
+
+app_name = 'profileapp'
+
+
 urlpatterns =[
+
+# 로직을 트리거할 주소
+
+    path('create/', ProfileCreateView.as_view(),name='create' ),
 
 
 
--- profileapp/views.py
+++ profileapp/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+from profileapp.forms import ProfileCreationForm
+from profileapp.models import Profile
+
+
+class ProfileCreateView(CreateView):
+
+    model = Profile
+# 어떤모델을 만들 것인지
+    form_class = ProfileCreationForm
+# 어떤 폼을 사용할 것인지
+    success_url = reverse_lazy('accountapp:hello_world')
+# 어디로 되돌아 갈것인지
+    template_name = 'profileapp/create.html'
+#  어떤 html 파일로 만들것인지
+
+
