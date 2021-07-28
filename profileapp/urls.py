4ebf362e6beb2760b9d31842646fc297361bfdaf
Jeong Da Un <wjekdns019@likelion.org>
2021-07-28 17:10:28 +0900

    7/28
     4ban Commit 1

--- profileapp/urls.py
+++ profileapp/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from profileapp.views import ProfileCreateView
+from profileapp.views import ProfileCreateView, ProfileUpdateView
 
 app_name = 'profileapp'
 
@@ -10,7 +10,7 @@ urlpatterns =[
 # 로직을 트리거할 주소
 
     path('create/', ProfileCreateView.as_view(),name='create'),
-
+    path('update/<int:pk>', ProfileUpdateView.as_view(),name='update'),
 
 
 ]
