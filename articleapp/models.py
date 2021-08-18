3574f4a66639af91cba05266d6be8796e8b4db49
Jeong Da Un <wjekdns019@likelion.org>
2021-08-18 17:08:18 +0900

    8/18 -2
     4ban Commit 1

--- articleapp/models.py
+++ articleapp/models.py
@@ -2,14 +2,18 @@ from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+from projectapp.models import Project
 
 
 class Article(models.Model):
     writer = models.ForeignKey(User, on_delete=models.SET_NULL, related_name='article',      # 접근할 이름 target_user.article 이렇게 됨
     null=True)
-    # 1대 다로 테이블과 테이블을 연결함  ,,, User 을 연결
-    # on_delete : user 가 삭제 되었을 떄 하는 것
-    # null 값도 ok 하다는 것임
+        # 1대 다로 테이블과 테이블을 연결함  ,,, User 을 연결
+        # on_delete : user 가 삭제 되었을 떄 하는 것
+        # null 값도 ok 하다는 것임
+
+    project = models.ForeignKey(Project, on_delete=models.SET_NULL,
+                                related_name='article', null=True)
 
     title = models.CharField(max_length=200, null=True)
     image =models.ImageField(upload_to='article/', null=True)
