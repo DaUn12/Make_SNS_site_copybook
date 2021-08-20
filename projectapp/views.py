5607b68c7cd55cf6d349c341034e9b3cfa76550c
Jeong Da Un <wjekdns019@likelion.org>
2021-08-20 17:51:06 +0900

    8/20 -3
     4ban Commit 1

--- projectapp/views.py
+++ projectapp/views.py
@@ -10,6 +10,8 @@ from django.views.generic.list import MultipleObjectMixin
 from articleapp.models import Article
 from projectapp.forms import ProjectCreationForm
 from projectapp.models import Project
+from subscribeapp.models import Subscription
+
 
 @method_decorator(login_required, 'get')
 @method_decorator(login_required, 'post')
@@ -31,8 +33,21 @@ class ProjectDetailView(DetailView, MultipleObjectMixin):
 
     # 객체의 리스트를 돌려줌
     def get_context_data(self, **kwargs):
+        # 어떤 유저가 어떤 게시물에 구독했는지 정의 시
+        user = self.request.user
+        project = self.object  # = target_project 와 같음
+
+        # 로그인 여부 확인
+        if user.is_authenticated:
+            subscription = Subscription.objects.filter(user=user,
+                                                       project=project)
+        else:
+            subscription = None
+
         article_list = Article.objects.filter(project=self.object)      # Article = DB
-        return super().get_context_data(object_list=article_list, **kwargs)
+        return super().get_context_data(object_list=article_list,
+                                        subscription=subscription,
+                                        **kwargs)
                         # object_list : 장고에서 지원해주는것
 
 class ProjectListView(ListView):
